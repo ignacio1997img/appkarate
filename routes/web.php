89f3cfa6c89e559fd69db09a62d1176c33e43031
ignacio1997img <ignaciomolinaguzman20@gmail.com>
2023-09-27 11:11:50 -0400

    si

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return redirect('admin/login');
+})->name('login');
 
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+// Route::get('/admin/clear-cache', function() {
+//     Artisan::call('optimize:clear');
+//     return redirect('/admin')->with(['message' => 'Cache eliminada.', 'alert-type' => 'success']);
+// })->name('clear.cache');
